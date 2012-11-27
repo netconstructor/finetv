a2d9c351eda0fdd5a316b722c818e1adddba0ddb
Johan Furuskog <johan.furuskog@finspang.se>
2012-11-27 08:59:04 +0100

    Created dynamic manifest instead

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -3,7 +3,7 @@
 <!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="sv" manifest="finetv.manifest"><!--<![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="sv" manifest="/finetv-manifest.php"><!--<![endif]-->
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
